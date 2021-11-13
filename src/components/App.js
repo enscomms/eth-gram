689f29b4a22b1b61e5ddef8e55a369a9177a262c
coutram <outram@gmail.com>
2021-11-13 10:32:44 -0500

    src and public folders with the boilerplate changes

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import logo from '../logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="http://www.icepop.ai/web3"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Web3 Communication
+          </a>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <a
+                  href="http://www.icepop.ai/web3"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={logo} className="App-logo" alt="logo" />
+                </a>
+                <h1>Web3 Communication</h1>
+                <p>
+                  Edit <code>src/components/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="http://www.icepop.ai/web3"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                 
+                </a>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
